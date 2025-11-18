005db7a99f0fab430064defdb975ffac148626f8
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2025-11-18 02:28:29 +0000

    New Commits

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
